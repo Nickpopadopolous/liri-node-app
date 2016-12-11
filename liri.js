aa223664149a20615b9d8188338da615c356e2dd
Nick Pearce <nicholas.pearce0910@gmail.com>
2016-12-11 00:36:26 -0500

    initial movie

--- liri.js
+++ liri.js
@@ -1,21 +1,38 @@
 var keys = require("./keys.js");
-
 var fs = require('fs');
-
-var action = process.argv[2];
-
+var Twitter = require('twitter');
+var Spotify = require('spotify');
 var request  = require("request");
+var inputOne = process.argv[2];
+var inputTwo = process.argv[3];
 
-var queryUrl = "http://www.omdbapi.com/?t=" + action + "&y=&plot=short&r=json";
+
+function switchOne() {
+	switch(inputOne) {
+		case "movie-this":
+		if(inputTwo == undefined){
+			inputTwo == 'Mr.Nobody';
+		}
+		movieFunction();
+		break;
+	}
+}
+
+function movieFunction()  {
+var queryUrl = "http://www.omdbapi.com/?t=" + inputTwo + "&y=&plot=short&tomatoes=true&r=json";
 request(queryUrl, function(error, response, body) {
 if (error) return console.log(error);
 
-	switch (action) {
-		
-		case 'movie-this':
 		body = JSON.parse(body)
+		console.log("Title: " + body.Title);
 		console.log("Release Year: " + body.Year);
-
-			break;
-		}
+		console.log("Country: " + body.Country);
+		console.log("Language: " + body.Language);
+		console.log("Plot: " + body.Plot);
+		console.log("Actors: " + body.Actors);
+		console.log("IMDB Rating " + body.imdbRating);
+		console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+		console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+		
 	});
+}
